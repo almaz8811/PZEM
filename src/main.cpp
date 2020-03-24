2c1f5df964730ddb3a045e02e8a8cb3f40b11474
AlMaz <almaz8811@gmail.com>
2020-03-24 21:11:04 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266SSDP.h>
+#include <FS.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPUpdateServer.h>
+#include <DNSServer.h>
+#include <TickerScheduler.h> // Библиотека тикета https://github.com/Toshik/TickerScheduler
+// Библиотеки устройств
+#include <time.h> // http://esp8266-arduinoide.ru/step8-timeupdate/
+#include <MQTT.h>
+#include <SPI.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
